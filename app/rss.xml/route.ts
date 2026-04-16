180fe79b160a5d55577eccdf60b518a05f1c5c7e
Yashwanth <yashwanths4209@gmail.com>
2026-04-16 13:21:33 +0530

    fixed rss using getAllBlogs

--- app/rss.xml/route.ts
+++ app/rss.xml/route.ts
@@ -1,30 +1,31 @@
-import { getAllPosts } from '@/lib/mdx';
+import { getAllBlogs } from "@/lib/mdx";
 
 export async function GET() {
-  const posts = await getAllPosts();
+  const posts = getAllBlogs();
 
   const rss = `<?xml version="1.0" encoding="UTF-8" ?>
-  <rss version="2.0">
-    <channel>
-      <title>Yashwanth's Valued Research Insights - Stock Market Analysis</title>
-      <link>https://yashwanths.in</link>
-      <description>Stock market insights and analysis</description>
-      ${posts
-        .map(
-          (post) => `
-        <item>
-          <title>${post.title}</title>
-          <link>https://yashwanths.in/blog/${post.slug}</link>
-          <description>${post.description}</description>
-        </item>`
-        )
-        .join('')}
-    </channel>
-  </rss>`;
+<rss version="2.0">
+<channel>
+  <title>Yashwanth's Valued Research Insights</title>
+  <link>https://yashwanths.in</link>
+  <description>Stock market insights and research</description>
+  ${posts
+    .map(
+      (post) => `
+    <item>
+      <title>${post.frontmatter.title}</title>
+      <link>https://yashwanths.in/blog/${post.frontmatter.slug}</link>
+      <description>${post.frontmatter.description}</description>
+      <pubDate>${new Date(post.frontmatter.date).toUTCString()}</pubDate>
+    </item>`
+    )
+    .join("")}
+</channel>
+</rss>`;
 
   return new Response(rss, {
     headers: {
-      'Content-Type': 'application/xml',
+      "Content-Type": "application/xml",
     },
   });
 }
